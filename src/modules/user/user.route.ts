c657ac4cf8f7cfe11fac73d35efc1176e8b0ae71
Nayeem Uddin <unayeem33@gmail.com>
2025-12-06 23:57:57 +0600

    s

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -5,5 +5,7 @@ import auth from "../../middleware/auth.js";
 const router = Router();
 
 router.get("/", auth(), userController.getAllUsers);
+router.put("/:userId", auth(), userController.updateUser);
+router.delete("/:userId", auth(), userController.deleteUser);
 
 export const userRoute = router;
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,11 +1,31 @@
 import { pool } from "../../database/db";
 
+// Get all users from the database
 const getAllUsersFromDB = async () => {
   const result = await pool.query(
     "SELECT id, name, email, phone, role FROM users"
   );
   return result;
 };
+
+// Update user in the database
+const updateUserInDB = async (payload: Record<string, unknown>, ID: string) => {
+  const { name, email, phone, role } = payload;
+
+  const result = await pool.query(
+    `UPDATE users SET name=$1, email=$2, phone=$3, role=$4 WHERE id=$5 RETURNING id, name, email, phone, role`,
+    [name, email, phone, role, ID]
+  );
+  return result;
+};
+
+const deleteUserFromDB = async (ID: string) => {
+  const result = await pool.query(`DELETE FROM users WHERE id=$1`, [ID]);
+  return result;
+};
+
 export const userServices = {
   getAllUsersFromDB,
+  updateUserInDB,
+  deleteUserFromDB,
 };
