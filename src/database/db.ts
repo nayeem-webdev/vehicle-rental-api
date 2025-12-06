33d43778d59e2e787b8ff9ec5d37e03165928a74
Nayeem Uddin <unayeem33@gmail.com>
2025-12-06 22:59:12 +0600

    s

--- src/database/db.ts
+++ src/database/db.ts
@@ -16,5 +16,16 @@ export const initDB = async () => {
         phone VARCHAR(11) NOT NULL,
         role VARCHAR(8) NOT NULL DEFAULT 'customer' CHECK (role IN ('customer', 'admin'))
     )`);
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS vehicles(
+        id SERIAL PRIMARY KEY,
+        vehicle_name VARCHAR(40) NOT NULL,
+        type VARCHAR(4) NOT NULL CHECK (type IN ('car', 'bike', 'van', 'suv')),
+        registration_number VARCHAR(100) UNIQUE NOT NULL,
+        daily_rent_price DECIMAL(10, 2) NOT NULL CHECK (daily_rent_price > 0),
+        availability_status VARCHAR(9) NOT NULL DEFAULT 'available' CHECK (availability_status IN ('available', 'booked'))    
+    )`);
+
   console.log("db connected");
 };
